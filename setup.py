39a4280c55ba50ace8d2a1e47988d6e8a30aadec
Zhang Lei <zhanglei@megvii.com>
2016-04-01 11:23:35 +0800

    mini update

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name="faceid newground",
+    name="faceid-newground",
     version="0.0.1",
     author="KAS Team in Megvii",
     author_email="kas@megvii.com",
     description="faceid newground",
-    packages=['newground'],
+    packages=['newground', 'newground/resources', 'newground/common'],
     long_description=read('README'),
 )
