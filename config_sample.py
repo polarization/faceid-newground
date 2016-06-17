91ad2b61bd24b4f3c68eb7859e92fa3c940843d0
leixun <leixun@megvii.com>
2016-06-17 18:14:08 +0800

    modify the config_sample.py

---
+++ config_sample.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+swift_endpoint = '10.101.2.9:8080'
+
+redis_IP = "10.101.2.9"
+redis_port = "6379"
+FDBInfo_rediskeyname = 'FDBInfo'
+SwiftAccountInfo_rediskeyname = 'SwiftAccountInfo'
+SWauth_Admin_User = '.super_admin'
+SWauth_Admin_Key = 'swauthkey'
